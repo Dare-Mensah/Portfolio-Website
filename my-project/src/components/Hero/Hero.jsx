ba0022cd59abb55064c683508829000af6397f4e
Dare-Mensah <91201253+Dare-Mensah@users.noreply.github.com>
2024-11-27 01:09:18 +0000

    added carousel

--- my-project/src/components/Hero/Hero.jsx
+++ my-project/src/components/Hero/Hero.jsx
@@ -5,7 +5,17 @@ import githubimg from "../../assets/github.jpg"
 import GTLogo from '../../assets/GTLogo.jpeg'
 import amazonLogo from '../../assets/Amazon-Logo-07.jpg'
 import microsoftLogo from '../../assets/microsoftLogo.png'
+
 import QueenMaryUniversityLogo from '../../assets/Queen-Mary-UniversityLogo.png'
+import ClimateSenseHomepagePhoneOne from '../../assets/ClimateSenseHomepagePhoneOne.jpg'
+import ClimateSenseHomepageTablet from '../../assets/ClimateSenseHomepageTablet.jpg'
+import ClimateSenseLoginScreen from '../../assets/ClimateSenseLoginScreen.jpg'
+import FDMWellbeingHomepageOne from'../../assets/FDMWellbeingHomepageOne.jpg'
+import FDMWellbeingHomepageTwo from'../../assets/FDMWellbeingHomepageTwo.jpg'
+
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Hero = () => {
     const Work_Experiences = [
@@ -56,6 +66,33 @@ const Hero = () => {
             logo: QueenMaryUniversityLogo,
             duration: 'Feb 2023 - Aug 2024'
         },
+    ]
+
+    const Projects =[
+        {
+            Title: 'Climate Sense',
+            image: ClimateSenseHomepagePhoneOne,
+            github_link: ''
+        },
+
+        {
+            Title: 'FDM WellbeingApp',
+            image: FDMWellbeingHomepageOne,
+            github_link: ''
+        },
+
+        {
+            Title: 'FDM WellbeingApp',
+            image: FDMWellbeingHomepageOne,
+            github_link: ''
+        },
+
+        {
+            Title: 'FDM WellbeingApp',
+            image: FDMWellbeingHomepageOne,
+            github_link: ''
+        },
+
     ]
 
 
@@ -179,36 +216,18 @@ const Hero = () => {
         <p className="text-stone-900 text-center">Browse My Recent</p>
         <h1 className="text-6xl font-bold text-center mb-10">Projects</h1>
         <div className="container flex flex-col items-center justify-center gap-11 mx-auto  bg-white w-full">
-            <div className="flex flex-wrap justify-center gap-3">
-            {Work_Experiences.map((Work_Experiences, index) => (
-                <div className="border-2 rounded-3xl p-3 w-96 h-70">
-                    <p className="text-center font-bold mb-3">{Work_Experiences.job_title}</p>
-                    <div className='flex space-x-4 grid-rows-2  justify-center'>
-                        <img src={Work_Experiences.logo} height={40} width={40}/>
-                        <p className="text-center mt-2">{Work_Experiences.company}</p>
-                    </div>
-                        <p className='text-center font-thin mt-3 text-neutral-800 antialiased'>{Work_Experiences.duration}</p>
+        <Carousel swipeable={true} showIndicators={true} showThumbs={true} showArrows={true}>
+        {Projects.map((Projects, index) => (
+            <div className="flex flex-wrap justify-center gap-3"> 
+                <div className="border-2 rounded-3xl p-3 w-96 h-70 justify-items-center">
+                    <img className='rounded-3xl items-center' src= {Projects.image} height={200} width={200}/>
                     <div>
-                    {Work_Experiences.description.map((text, textIndex) => (
-                        <div key={textIndex}>
-                            <ol className=' list-disc pl-5'>
-                                <li className='mt-4'>{Work_Experiences.description}</li>
-                            </ol>
-                        </div>
-                    ))}
+                        <h2 className='text-3xl font-bold text-center my-4'>{Projects.Title}</h2>
                     </div>
                 </div>
-            ))}
-        </div>
-        <div>
-            <p className="max-w-full text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit enim
-                inventore incidunt cupiditate ducimus aut ullam, veritatis ex
-                doloremque fuga? Lorem ipsum dolor sit, amet consectetur adipisicing
-                elit. Reiciendis accusantium sint aut perspiciatis accusamus. Hic ea
-                harum voluptatibus commodi minus maxime at.
-            </p>
-        </div>
+            </div>
+        ))}
+        </Carousel>
         </div>
     </section>
 
